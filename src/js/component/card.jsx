09049afc0722c8ad1f58f457c0338ff49466dde0
Mvferrof <mvferrof@gmail.com>
2023-01-25 15:39:22 +0000

    first commit

---
+++ src/js/component/card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Images from "../../img/pink-324175_1280.jpg"
+const Card = (props) => {
+    return (
+      <div className="card col-3 mt-3 border padding">
+        <img src={props.item.imgUrl} className="imageSize" id="square1" alt=""/>
+        <h4 className=" mt-3 text-dark">{props.item.title}</h4>
+        <p>{props.item.description}</p>
+        <div className="mt-3 border-top marginBottom">
+          <button type="button" className="btn btn-primary">Find Out More!</button>
+        </div>
+      </div>
+    )
+}
+export default Card;
